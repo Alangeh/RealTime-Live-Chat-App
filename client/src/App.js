8d6a82beb18426ee9e920d24f0b0b236dbbc7226
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-11-25 23:04:04 +0300

    client side bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import ChatHistory from './components/ChatHistory/ChatHistory';
 import ChatInput from './components/ChatInput/ChatInput';
-import '/App.css';
+import './App.css';
 import { connect, sendMsg } from './api';
 
 class App extends Component {
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Header.scss';
+import './Header.css';
 
 const Header = () => {
     <div className='header'>
